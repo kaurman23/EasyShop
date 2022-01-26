b3933dffe6b266d5cf3d22737590942286dc41ee
Manpreet Kaur <kaurman2305@gmail.com>
2022-01-27 00:28:43 +0530

    Created the product page

--- frontend/src/Products/pages/Product.js
+++ frontend/src/Products/pages/Product.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React from 'react'
+
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { Link, useParams } from 'react-router-dom'
+import products from '../../products'
+import Rating from '../../shared/UI/Rating'
 
 const Product = () => {
-  return <div>Products</div>;
-};
+  let { id } = useParams()
 
-export default Product;
+  const product = products.find((product) => product._id === id)
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        <b>Go back</b>
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item><h3 className="my-3">{product.name}</h3></ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} ratings`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+            <Card>
+            <ListGroup variant="flush">
+                <ListGroup.Item><Row><Col>Price:</Col><Col>${product.price}</Col></Row></ListGroup.Item>
+                <ListGroup.Item><Row><Col>Status:</Col><Col>{product.countInStock?'In Stock':'Out of stock'}</Col></Row></ListGroup.Item>
+                <ListGroup.Item><Button className='w-100' type="button" disabled={product.countInStock === 0}>ADD TO CART</Button></ListGroup.Item>
+            </ListGroup>
+            </Card>
+            
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default Product
