b3e901cd439ec80e760b7eecdbf557b3ac5d02a3
Manpreet Kaur <kaurman2305@gmail.com>
2022-01-27 21:10:07 +0530

    Added concurrently, nodemon and dot env files

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -17,6 +20,8 @@ app.get('/api/products/:id', (req, res, next) => {
   res.json(product)
 })
 
-app.listen(5000, () => {
-  console.log('Server is running at port 5000')
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server is running in ${process.env.NODE_ENV} at port ${PORT}`)
 })
--- frontend/src/Products/pages/Product.js
+++ frontend/src/Products/pages/Product.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 
-import products from '../../products'
 import Rating from '../../shared/UI/Rating'
 import axios from 'axios'
 
